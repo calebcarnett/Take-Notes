9545849d26e898899fe1c347763503cddb1aa3b1
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2022-12-27 12:29:16 -0500

    test

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = ('/public/notes.html')
+const express = require('express');
+const path = require('path');
 
-app.get('/public/notes.html', (req, res) => {
-  res.send('heo')
-})
+const PORT = 3001;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
