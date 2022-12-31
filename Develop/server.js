95f6a0f5f68cd695bcddda3708e5385cce27738c
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2022-12-31 09:29:54 -0500

    added a post request, note is saving to an undefined.json file in the db folder. next step is to figure out how to correctly use a post request to save data.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
+const notes = require('./db/notes');
 const PORT = 3001;
 
 const app = express();
@@ -13,10 +14,53 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+  console.info(`GET /api/notes`);
+  res.status(200).json(notes);
 });
 
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text
+    };
+
+    const noteString = JSON.stringify(newNote);
+ 
+    fs.writeFile(`./db/${newNote.product}.json`, noteString, (err) =>
+    err
+      ? console.error(err)
+      : console.log(
+          `Review for ${newNote.product} has been written to JSON file`
+        )
+  );
+  const response = {
+    status: 'success',
+    body: newNote,
+  };
+
+  console.log(response);
+  res.status(201).json(response);
+} else {
+  res.status(500).json('Error in posting review');
+}
+});
+
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`app listening at http://localhost:${PORT} 🚀`)
 })
