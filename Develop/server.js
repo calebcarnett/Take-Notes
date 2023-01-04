4ca3421c8a95daf113afa72a9a82335af44c991c
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-01-04 10:39:06 -0500

    added some notes to my server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,61 +1,63 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 const PORT = 3001;
+// Helper method for generating unique ids
+const uuid = require("./helper/uuid");
 
 const app = express();
-
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+app.use(express.static("public"));
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+// GET Route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-);
-
-const readFromFile = util.promisify(fs.readFile);
-
-app.get('/api/notes', (req, res) => {
+// GET Route for retrieving all notes
+app.get("/api/notes", (req, res) => {
   console.info(`${req.method} request received for notes`);
-readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
+  readFromFile("./db/notes.json").then((data) => res.json(JSON.parse(data)));
 });
 
-
-app.post('/api/notes', (req, res) => {
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+// POST Route for a new notes
+app.post("/api/notes", (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
 
-  // // Destructuring assignment for the items in req.body
+  // Destructuring for the items in req.body
   const { title, text } = req.body;
-console.log('the body', req.body);
+  console.log("the body", req.body);
 
-  // // If all the required properties are present
+  // If all the required properties are present
   if (title && text) {
     // Variable for the object we will save
     const newNote = {
       title,
-      text
+      text,
+      id: uuid(),
     };
-
-  const data = JSON.parse(fs.readFileSync('./db/notes.json', "utf8"))
-
-  data.push(newNote);
-
- fs.writeFileSync('./db/notes.json', JSON.stringify(data))
-  res.status(201).json(data);
-  
-} else {
-  res.status(500).json('Error in posting review');
-}
+    //converted notes.json into an object and assigned a data constant
+    const data = JSON.parse(fs.readFileSync("./db/notes.json", "utf8"));
+    //pushed the newly added note to notes.json file
+    data.push(newNote);
+    //writes to file notes.json, stringify changes the data in the object to a string
+    fs.writeFileSync("./db/notes.json", JSON.stringify(data));
+    res.status(201).json(data);
+  } else {
+    res.status(500).json("Error in posting review");
+  }
 });
 
-
 app.listen(PORT, () => {
-  console.log(`app listening at http://localhost:${PORT} 🚀`)
-})
+  console.log(`app listening at http://localhost:${PORT} 🚀`);
+});
